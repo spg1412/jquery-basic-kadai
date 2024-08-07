0563f3bca056cc632b51d8e9a7fe0081fa069700
spg1412 <terransullivan@hotmail.com>
2024-08-08 07:33:18 +0900

    kadai_008

---
+++ kadai_008/form.js
@@ -0,0 +1,7 @@
+$(function () {
+    //Make the button function
+    $('.btn').on('click', function () {
+        //When you click, say it clicks in the box
+        $('.text-box').val('クリックしました！');
+    });
+});
