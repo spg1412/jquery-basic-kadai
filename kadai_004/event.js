7960d0be6f82816e091c764ae4d00a67c81a57ce
spg1412 <terransullivan@hotmail.com>
2024-08-08 07:19:05 +0900

    kadai_004

---
+++ kadai_004/event.js
@@ -0,0 +1,8 @@
+// Scroll Console Function
+$(document).on('scroll', function() {
+    console.log('scrollイベントが発生しました');
+});
+// Finish Loading Function
+$(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+});
